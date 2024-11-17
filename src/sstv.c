455472c320a567c8a4e468550502f2f376881004
Stuart Longland <me@vk4msl.com>
2024-11-17 22:43:01 +1000

    sstv: Implement Scottie modes
    Timings might need some tweaking, but we get decodes in QSSTV and slowrx
    with some slant.

--- src/sstv.c
+++ src/sstv.c
@@ -42,6 +42,29 @@ const static struct sstvenc_encoder_pulse sstvenc_sstv_robotbw_fp[] = {
     {.frequency = 0, .duration_ns = 0},
 };
 
+/* SSTV mode specifications -- Scottie modes */
+const static struct sstvenc_encoder_pulse sstvenc_sstv_scottie_fp[] = {
+    {.frequency = SSTVENC_FREQ_BLACK, .duration_ns = 1500000},
+    {.frequency = 0, .duration_ns = 0},
+};
+
+const static struct sstvenc_encoder_pulse sstvenc_sstv_scottie_sep01[] = {
+    {.frequency = SSTVENC_FREQ_BLACK, .duration_ns = 3000000},
+    {.frequency = 0, .duration_ns = 0},
+};
+
+const static struct sstvenc_encoder_pulse sstvenc_sstv_scottie_sep12[] = {
+    {.frequency = SSTVENC_FREQ_BLACK, .duration_ns = 1500000},
+    {.frequency = SSTVENC_FREQ_SYNC, .duration_ns = 9000000},
+    {.frequency = SSTVENC_FREQ_BLACK, .duration_ns = 1500000},
+    {.frequency = 0, .duration_ns = 0},
+};
+
+const static struct sstvenc_encoder_pulse sstvenc_sstv_scottie_bp[] = {
+    {.frequency = SSTVENC_FREQ_BLACK, .duration_ns = 1500000},
+    {.frequency = 0, .duration_ns = 0},
+};
+
 /* SSTV mode specifications -- Martin modes */
 const static struct sstvenc_encoder_pulse sstvenc_sstv_martin_fp[] = {
     {.frequency = SSTVENC_FREQ_SYNC, .duration_ns = 4862000},
@@ -125,6 +148,58 @@ const static struct sstvenc_mode sstvenc_sstv_modes[] = {
 	= SSTVENC_MODE_ORDER(SSTVENC_CSO_MODE_MONO, SSTVENC_CSO_CH_Y,
 			     SSTVENC_CSO_CH_NONE, SSTVENC_CSO_CH_NONE),
 	.vis_code = 0x0a,
+    },
+    /* Scottie modes */
+    {
+	.description	    = "Scottie S1",
+	.name		    = "S1",
+	.initseq	    = NULL,
+	.frontporch	    = sstvenc_sstv_scottie_fp,
+	.gap01		    = sstvenc_sstv_scottie_sep01,
+	.gap12		    = sstvenc_sstv_scottie_sep12,
+	.backporch	    = sstvenc_sstv_scottie_bp,
+	.finalseq	    = NULL,
+	.scanline_period_ns = {136740000, 136740000, 136740000},
+	.width		    = 320,
+	.height		    = 256,
+	.colour_space_order
+	= SSTVENC_MODE_ORDER(SSTVENC_CSO_MODE_RGB, SSTVENC_CSO_CH_G,
+			     SSTVENC_CSO_CH_B, SSTVENC_CSO_CH_R),
+	.vis_code = 0x3c,
+    },
+    {
+	.description	    = "Scottie S2",
+	.name		    = "S2",
+	.initseq	    = NULL,
+	.frontporch	    = sstvenc_sstv_scottie_fp,
+	.gap01		    = sstvenc_sstv_scottie_sep01,
+	.gap12		    = sstvenc_sstv_scottie_sep12,
+	.backporch	    = sstvenc_sstv_scottie_bp,
+	.finalseq	    = NULL,
+	.scanline_period_ns = {86564000, 86564000, 86564000},
+	.width		    = 320,
+	.height		    = 256,
+	.colour_space_order
+	= SSTVENC_MODE_ORDER(SSTVENC_CSO_MODE_RGB, SSTVENC_CSO_CH_G,
+			     SSTVENC_CSO_CH_B, SSTVENC_CSO_CH_R),
+	.vis_code = 0x38,
+    },
+    {
+	.description	    = "Scottie DX",
+	.name		    = "SDX",
+	.initseq	    = NULL,
+	.frontporch	    = sstvenc_sstv_scottie_fp,
+	.gap01		    = sstvenc_sstv_scottie_sep01,
+	.gap12		    = sstvenc_sstv_scottie_sep12,
+	.backporch	    = sstvenc_sstv_scottie_bp,
+	.finalseq	    = NULL,
+	.scanline_period_ns = {344100000, 344100000, 344100000},
+	.width		    = 320,
+	.height		    = 256,
+	.colour_space_order
+	= SSTVENC_MODE_ORDER(SSTVENC_CSO_MODE_RGB, SSTVENC_CSO_CH_G,
+			     SSTVENC_CSO_CH_B, SSTVENC_CSO_CH_R),
+	.vis_code = 0x4c,
     },
     /* Martin modes */
     {
