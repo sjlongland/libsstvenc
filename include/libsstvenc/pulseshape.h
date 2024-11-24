6c616db626070af1daaae1dca65637ef5f97ed4a
Stuart Longland <me@vk4msl.com>
2024-11-24 18:04:04 +1000

    pulseshape: Document module

--- include/libsstvenc/pulseshape.h
+++ include/libsstvenc/pulseshape.h
@@ -2,9 +2,11 @@
 #define _SSTVENC_PULSESHAPE_H
 
 /*!
- * Pulse shaper.  The purpose of this module is to drive the amplitude of
- * an oscillator to ensure the waveform generated contains as few artefacts
- * as possible.
+ * @defgroup pulseshape Asynchronous Pulse Shaper.
+ * @{
+ *
+ * The purpose of this module is to drive the amplitude of an oscillator to
+ * ensure the waveform generated contains as few artefacts as possible.
  */
 
 /*
@@ -16,22 +18,69 @@
 #include <math.h>
 #include <stdint.h>
 
-/*
- * Pulse shape phases
+/*!
+ * @defgroup pulseshape_states Pulse Shaper States
+ * @{
  */
-#define SSTVENC_PS_PHASE_INIT	 (0)
-#define SSTVENC_PS_PHASE_RISE	 (1)
-#define SSTVENC_PS_PHASE_HOLD	 (2)
-#define SSTVENC_PS_PHASE_FALL	 (3)
-#define SSTVENC_PS_PHASE_DONE	 (4)
 
 /*!
- * Hold time = infinite
+ * Initial state.  No samples have been emitted yet.  @ref sstvenc_ps_compute
+ * immediately moves the state machine to SSTVENC_PS_PHASE_RISE and handles
+ * the machine accordingly.
+ */
+#define SSTVENC_PS_PHASE_INIT	 (0)
+
+/*!
+ * Rising slope.  The amplitude is being ramped up to maximum.
+ *
+ * The counter sstvenc_pulseshape#sample_idx is used to track the rise.  When
+ * it reaches or exceeds sstvenc_pulseshape#rise_sz, the state machine resets
+ * the counter and advances to SSTVENC_PS_PHASE_HOLD.
+ */
+#define SSTVENC_PS_PHASE_RISE	 (1)
+
+/*!
+ * Hold state.  We maintain the pulse amplitude at its maximum for the
+ * duration of the pulse.
+ *
+ * The counter sstvenc_pulseshape#sample_idx is used to track the time period
+ * spent at maximum amplitude.  If sstvenc_pulseshape#hold_sz is finite, the
+ * counter is reset and the state machine advanced to SSTVENC_PS_PHASE_FALL
+ * once it reaches or exceeds sstvenc_pulseshape#hold_sz.
+ *
+ * Otherwise, it will stay in this state indefinitely, code must call
+ * @ref sstvenc_ps_advance manually to exit the hold state.
+ */
+#define SSTVENC_PS_PHASE_HOLD	 (2)
+
+/*!
+ * Falling slope.  The amplitude is being wound down to zero.
+ *
+ * The counter sstvenc_pulseshape#sample_idx is used to track the fall.  When
+ * it reaches or exceeds sstvenc_pulseshape#fall_sz, the state machine resets
+ * the counter and advances to SSTVENC_PS_PHASE_DONE.
+ */
+#define SSTVENC_PS_PHASE_FALL	 (3)
+
+/*!
+ * Pulse finished.  The state machine will reset
+ * sstvenc_pulseshape#sample_idx to zero on entering this state and will
+ * increment it each sample whilst keeping sstvenc_pulseshape#output at
+ * zero.
+ */
+#define SSTVENC_PS_PHASE_DONE	 (4)
+/*!
+ * @}
+ */
+
+/*!
+ * Hold time = infinite.
  */
 #define SSTVENC_PS_HOLD_TIME_INF SSTVENC_TS_INFINITE
 
 /*!
- * Pulse shaper data structure.
+ * Pulse shaper data structure.  This should be initialised by calling
+ * @ref sstvenc_ps_init.
  */
 struct sstvenc_pulseshape {
 	/*! Peak amplitude of the pulse */
@@ -40,31 +89,25 @@ struct sstvenc_pulseshape {
 	double	 output;
 	/*! Sample rate for the pulse in Hz */
 	uint32_t sample_rate;
-	/*!
-	 * Sample index for the current phase.
-	 */
+	/*! Sample index for the current phase. */
 	uint32_t sample_idx;
-	/*!
-	 * Number of samples for the hold phase.
-	 */
+	/*! Number of samples for the hold phase. */
 	uint32_t hold_sz;
-	/*!
-	 * Number of samples for the rising pulse.
-	 */
+	/*! Number of samples for the rising pulse. */
 	uint16_t rise_sz;
-	/*!
-	 * Number of samples for the falling pulse.
-	 */
+	/*! Number of samples for the falling pulse. */
 	uint16_t fall_sz;
-	/*!
-	 * Current pulse shaper phase.
-	 */
+	/*! Current pulse shaper phase. */
 	uint8_t	 phase;
 };
 
 /*!
  * Reset the pulse shape state machine with a new hold time given in samples,
  * but otherwise identical settings.
+ *
+ * @param[inout]	ps		Pulse shaper context being reset
+ * @param[in]		hold_time	New hold time (given as number of
+ * 					samples).
  */
 static inline void
 sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
@@ -77,6 +120,13 @@ sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
 /*!
  * Reset the pulse shape state machine with a new hold time, but otherwise
  * identical settings.
+ *
+ * @param[inout]	ps		Pulse shaper context being reset
+ * @param[in]		hold_time	New hold time given in the time unit
+ * 					specified by @a time_unit.
+ * @param[in]		time_unit	Time unit used to measure @a
+ * hold_time. Must be one of the values given in
+ * 					@ref timescale_units
  */
 static inline void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps,
 				    double hold_time, uint8_t time_unit) {
@@ -91,12 +141,14 @@ static inline void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps,
  * @param[out]	ps		The pulse shaper being initialised
  * @param[in]	amplitude	The peak amplitude for the pulse shaper
  * @param[in]	rise_time	The rise time in seconds, use 0 to disable
- * rise.
+ * 				rise.
  * @param[in]	hold_time	The hold time in seconds, use INFINITY for
  * 				an indefinite hold.
  * @param[in]	fall_time	The fall time in seconds, use 0 to disable
- * fall.
+ * 				fall.
  * @param[in]	sample_rate	The sample rate in Hz.
+ * @param[in]	time_unit	The time unit used to measure @a rise_time,
+ * 				@a hold_time and @a fall_time.
  */
 static inline void sstvenc_ps_init(struct sstvenc_pulseshape* const ps,
 				   double amplitude, double rise_time,
@@ -130,9 +182,11 @@ static inline void sstvenc_ps_init(struct sstvenc_pulseshape* const ps,
 /*!
  * Advance the pulse shaper to the next phase, regardless of whether it is
  * finished with the present one.  A no-op at the "done" phase.
+ *
+ * @param[out]	ps		The pulse shaper being advanced.
  */
 static inline void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps) {
-	if (ps->phase <= SSTVENC_PS_PHASE_DONE) {
+	if (ps->phase < SSTVENC_PS_PHASE_DONE) {
 		ps->sample_idx = 0;
 		ps->phase++;
 	}
@@ -141,6 +195,10 @@ static inline void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps) {
 /*!
  * Compute the next pulse shaper value and store it in the output field.
  * A no-op if the sample rate is set to zero or the pulse shaper is done.
+ *
+ * The output envelope amplitude can be read from sstvenc_pulseshape#output.
+ *
+ * @param[out]	ps		The pulse shaper being computed.
  */
 static inline void sstvenc_ps_compute(struct sstvenc_pulseshape* const ps) {
 	ps->sample_idx++;
@@ -197,5 +255,7 @@ static inline void sstvenc_ps_compute(struct sstvenc_pulseshape* const ps) {
 		break;
 	}
 }
+
+/*! @} */
 
 #endif
