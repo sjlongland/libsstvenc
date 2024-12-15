08bbedf07e79e2d6b292cb708e59802ce3e7e078
Stuart Longland <me@vk4msl.com>
2024-12-15 10:35:19 +1000

    pulseshape: De-inline implementation

--- include/libsstvenc/pulseshape.h
+++ include/libsstvenc/pulseshape.h
@@ -109,13 +109,8 @@ struct sstvenc_pulseshape {
  * @param[in]		hold_time	New hold time (given as number of
  * 					samples).
  */
-static inline void
-sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
-			 uint32_t			  hold_time) {
-	ps->phase      = SSTVENC_PS_PHASE_INIT;
-	ps->hold_sz    = hold_time;
-	ps->sample_idx = 0;
-}
+void sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
+			      uint32_t			       hold_time);
 
 /*!
  * Reset the pulse shape state machine with a new hold time, but otherwise
@@ -128,12 +123,8 @@ sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
  * hold_time. Must be one of the values given in
  * 					@ref timescale_units
  */
-static inline void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps,
-				    double hold_time, uint8_t time_unit) {
-	uint32_t samples = sstvenc_ts_unit_to_samples(
-	    hold_time, ps->sample_rate, time_unit);
-	sstvenc_ps_reset_samples(ps, samples);
-}
+void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps, double hold_time,
+		      uint8_t time_unit);
 
 /*!
  * Initialise a pulse shaper.
@@ -150,34 +141,9 @@ static inline void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps,
  * @param[in]	time_unit	The time unit used to measure @a rise_time,
  * 				@a hold_time and @a fall_time.
  */
-static inline void sstvenc_ps_init(struct sstvenc_pulseshape* const ps,
-				   double amplitude, double rise_time,
-				   double hold_time, double fall_time,
-				   uint32_t sample_rate, uint8_t time_unit) {
-	uint32_t samples;
-
-	ps->amplitude	= amplitude;
-	ps->sample_rate = sample_rate;
-	ps->output	= 0.0;
-
-	samples
-	    = sstvenc_ts_unit_to_samples(rise_time, sample_rate, time_unit);
-	if (samples > UINT16_MAX) {
-		ps->rise_sz = UINT16_MAX;
-	} else {
-		ps->rise_sz = samples;
-	}
-
-	samples
-	    = sstvenc_ts_unit_to_samples(fall_time, sample_rate, time_unit);
-	if (samples > UINT16_MAX) {
-		ps->fall_sz = UINT16_MAX;
-	} else {
-		ps->fall_sz = samples;
-	}
-
-	sstvenc_ps_reset(ps, hold_time, time_unit);
-}
+void sstvenc_ps_init(struct sstvenc_pulseshape* const ps, double amplitude,
+		     double rise_time, double hold_time, double fall_time,
+		     uint32_t sample_rate, uint8_t time_unit);
 
 /*!
  * Advance the pulse shaper to the next phase, regardless of whether it is
@@ -185,12 +151,7 @@ static inline void sstvenc_ps_init(struct sstvenc_pulseshape* const ps,
  *
  * @param[out]	ps		The pulse shaper being advanced.
  */
-static inline void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps) {
-	if (ps->phase < SSTVENC_PS_PHASE_DONE) {
-		ps->sample_idx = 0;
-		ps->phase++;
-	}
-}
+void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps);
 
 /*!
  * Compute the next pulse shaper value and store it in the output field.
@@ -200,61 +161,7 @@ static inline void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps) {
  *
  * @param[out]	ps		The pulse shaper being computed.
  */
-static inline void sstvenc_ps_compute(struct sstvenc_pulseshape* const ps) {
-	ps->sample_idx++;
-
-	switch (ps->phase) {
-	case SSTVENC_PS_PHASE_INIT:
-		ps->phase++;
-		/* Fall-thru */
-	case SSTVENC_PS_PHASE_RISE:
-		if (ps->rise_sz) {
-			/* Compute the raised sine amplitude */
-			ps->output = ps->amplitude
-				     * (1.0
-					- cos((ps->sample_idx * M_PI)
-					      / (2 * ((double)ps->rise_sz))));
-			if (ps->output > ps->amplitude) {
-				ps->output = ps->amplitude;
-			}
-		}
-		if (ps->sample_idx > ps->rise_sz) {
-			/* Next phase */
-			sstvenc_ps_advance(ps);
-		}
-		break;
-	case SSTVENC_PS_PHASE_HOLD:
-		ps->output = ps->amplitude;
-		if ((ps->hold_sz != SSTVENC_PS_HOLD_TIME_INF)
-		    && (ps->sample_idx >= ps->hold_sz)) {
-			/* Next phase */
-			sstvenc_ps_advance(ps);
-		}
-		break;
-	case SSTVENC_PS_PHASE_FALL:
-		if (ps->fall_sz) {
-			/* Compute the raised sine amplitude */
-			ps->output
-			    = ps->amplitude
-			      * (1.0
-				 - cos(((ps->fall_sz - ps->sample_idx) * M_PI)
-				       / (2 * ((double)ps->fall_sz))));
-
-			if (ps->output > ps->amplitude) {
-				ps->output = ps->amplitude;
-			}
-		}
-		if (ps->sample_idx >= ps->fall_sz) {
-			/* Next phase */
-			sstvenc_ps_advance(ps);
-		}
-		break;
-	case SSTVENC_PS_PHASE_DONE:
-	default:
-		ps->output = 0.0;
-		break;
-	}
-}
+void sstvenc_ps_compute(struct sstvenc_pulseshape* const ps);
 
 /*! @} */
 
---
+++ src/pulseshape.c
@@ -0,0 +1,118 @@
+/*!
+ * @addtogroup pulseshape
+ * @{
+ */
+
+/*
+ * © Stuart Longland VK4MSL
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <libsstvenc/pulseshape.h>
+
+void sstvenc_ps_reset_samples(struct sstvenc_pulseshape* const ps,
+			      uint32_t			       hold_time) {
+	ps->phase      = SSTVENC_PS_PHASE_INIT;
+	ps->hold_sz    = hold_time;
+	ps->sample_idx = 0;
+}
+
+void sstvenc_ps_reset(struct sstvenc_pulseshape* const ps, double hold_time,
+		      uint8_t time_unit) {
+	uint32_t samples = sstvenc_ts_unit_to_samples(
+	    hold_time, ps->sample_rate, time_unit);
+	sstvenc_ps_reset_samples(ps, samples);
+}
+
+void sstvenc_ps_init(struct sstvenc_pulseshape* const ps, double amplitude,
+		     double rise_time, double hold_time, double fall_time,
+		     uint32_t sample_rate, uint8_t time_unit) {
+	uint32_t samples;
+
+	ps->amplitude	= amplitude;
+	ps->sample_rate = sample_rate;
+	ps->output	= 0.0;
+
+	samples
+	    = sstvenc_ts_unit_to_samples(rise_time, sample_rate, time_unit);
+	if (samples > UINT16_MAX) {
+		ps->rise_sz = UINT16_MAX;
+	} else {
+		ps->rise_sz = samples;
+	}
+
+	samples
+	    = sstvenc_ts_unit_to_samples(fall_time, sample_rate, time_unit);
+	if (samples > UINT16_MAX) {
+		ps->fall_sz = UINT16_MAX;
+	} else {
+		ps->fall_sz = samples;
+	}
+
+	sstvenc_ps_reset(ps, hold_time, time_unit);
+}
+
+void sstvenc_ps_advance(struct sstvenc_pulseshape* const ps) {
+	if (ps->phase < SSTVENC_PS_PHASE_DONE) {
+		ps->sample_idx = 0;
+		ps->phase++;
+	}
+}
+
+void sstvenc_ps_compute(struct sstvenc_pulseshape* const ps) {
+	ps->sample_idx++;
+
+	switch (ps->phase) {
+	case SSTVENC_PS_PHASE_INIT:
+		ps->phase++;
+		/* Fall-thru */
+	case SSTVENC_PS_PHASE_RISE:
+		if (ps->rise_sz) {
+			/* Compute the raised sine amplitude */
+			ps->output = ps->amplitude
+				     * (1.0
+					- cos((ps->sample_idx * M_PI)
+					      / (2 * ((double)ps->rise_sz))));
+			if (ps->output > ps->amplitude) {
+				ps->output = ps->amplitude;
+			}
+		}
+		if (ps->sample_idx > ps->rise_sz) {
+			/* Next phase */
+			sstvenc_ps_advance(ps);
+		}
+		break;
+	case SSTVENC_PS_PHASE_HOLD:
+		ps->output = ps->amplitude;
+		if ((ps->hold_sz != SSTVENC_PS_HOLD_TIME_INF)
+		    && (ps->sample_idx >= ps->hold_sz)) {
+			/* Next phase */
+			sstvenc_ps_advance(ps);
+		}
+		break;
+	case SSTVENC_PS_PHASE_FALL:
+		if (ps->fall_sz) {
+			/* Compute the raised sine amplitude */
+			ps->output
+			    = ps->amplitude
+			      * (1.0
+				 - cos(((ps->fall_sz - ps->sample_idx) * M_PI)
+				       / (2 * ((double)ps->fall_sz))));
+
+			if (ps->output > ps->amplitude) {
+				ps->output = ps->amplitude;
+			}
+		}
+		if (ps->sample_idx >= ps->fall_sz) {
+			/* Next phase */
+			sstvenc_ps_advance(ps);
+		}
+		break;
+	case SSTVENC_PS_PHASE_DONE:
+	default:
+		ps->output = 0.0;
+		break;
+	}
+}
+
+/*! @} */
