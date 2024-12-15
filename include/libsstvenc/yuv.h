4d786ccd03d6008d864d00bf6385e11e68ffc39d
Stuart Longland <me@vk4msl.com>
2024-12-15 10:46:55 +1000

    yuv: De-inline functions

--- include/libsstvenc/yuv.h
+++ include/libsstvenc/yuv.h
@@ -31,13 +31,7 @@
  *
  * @returns	Y component in Q8 fixed-point.
  */
-static inline uint8_t sstvenc_yuv_calc_y(uint8_t r, uint8_t g, uint8_t b) {
-	return (16.0
-		+ (.003906
-		   * ((65.738 * (double)r) + (129.057 * (double)g)
-		      + (25.064 * (double)b))))
-	       + 0.5;
-}
+uint8_t sstvenc_yuv_calc_y(uint8_t r, uint8_t g, uint8_t b);
 
 /*!
  * Return the U (red - luminance) component of a RGB colour.
@@ -48,13 +42,7 @@ static inline uint8_t sstvenc_yuv_calc_y(uint8_t r, uint8_t g, uint8_t b) {
  *
  * @returns	U component in Q8 fixed-point.
  */
-static inline uint8_t sstvenc_yuv_calc_u(uint8_t r, uint8_t g, uint8_t b) {
-	return (128.0
-		+ (.003906
-		   * ((112.439 * (double)r) + (-94.154 * (double)g)
-		      + (-18.285 * (double)b))))
-	       + 0.5;
-}
+uint8_t sstvenc_yuv_calc_u(uint8_t r, uint8_t g, uint8_t b);
 
 /*!
  * Return the V (blue - luminance) component of a RGB colour.
@@ -65,13 +53,7 @@ static inline uint8_t sstvenc_yuv_calc_u(uint8_t r, uint8_t g, uint8_t b) {
  *
  * @returns	V component in Q8 fixed-point.
  */
-static inline uint8_t sstvenc_yuv_calc_v(uint8_t r, uint8_t g, uint8_t b) {
-	return (128.0
-		+ (.003906
-		   * ((-37.945 * (double)r) + (-74.494 * (double)g)
-		      + (112.439 * (double)b))))
-	       + 0.5;
-}
+uint8_t sstvenc_yuv_calc_v(uint8_t r, uint8_t g, uint8_t b);
 
 /*!
  * @}
